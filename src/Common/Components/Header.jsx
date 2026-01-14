bad364f0bdae27d61a953219ea3c53222c157f5d
akshara-45-ux <akshara707@gmail.com>
2026-01-14 17:15:08 +0530

    style headersection login button

--- src/Common/Components/Header.jsx
+++ src/Common/Components/Header.jsx
@@ -2,52 +2,34 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
-import Form from 'react-bootstrap/Form'
-import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function Header() {
   return (
-    <Navbar bg="primary" variant="dark" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Container fluid>
+        <div className=' d-flex gap-3 mx-auto ms-2' >
+        <img src="https://clipground.com/images/blood-donation-png-17.png"alt=""  width="55px"
+  height="55px"
+  className="p-1 bg-dark rounded-circle"/>
         <Navbar.Brand as={NavLink} to="/">
-          MyApp
+          BloodNet
         </Navbar.Brand>
-
-        <Navbar.Toggle aria-controls="navbarScroll" />
-
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto my-2 my-lg-0" navbarScroll>
-            <Nav.Link as={NavLink} to="/">
-              Home
-            </Nav.Link>
-
-            <Nav.Link as={NavLink} to="/about">
-              About
-            </Nav.Link>
-
-            <NavDropdown title="Services" id="navbarScrollingDropdown">
-              <NavDropdown.Item as={NavLink} to="/service1">
-                Service 1
-              </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/service2">
-                Service 2
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            <Nav.Link disabled>Disabled</Nav.Link>
-          </Nav>
-
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-            />
-            <Button variant="outline-light">Search</Button>
-          </Form>
-        </Navbar.Collapse>
+        </div>
+<DropdownButton
+      id="search-dropdown"
+      title="Login"
+      variant="outline-light"
+      align="end"
+    >
+      <Dropdown.Item className="text-dark">Admin</Dropdown.Item>
+      <Dropdown.Item className="text-dark">User</Dropdown.Item>
+     
+    </DropdownButton>
+         
+     
       </Container>
     </Navbar>
   )
